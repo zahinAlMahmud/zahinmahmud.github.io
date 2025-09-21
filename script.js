7944d277f39dbf47d2fce74901650827e4ce9230
zahinAlMahmud <167842947+zahinAlMahmud@users.noreply.github.com>
2025-09-22 01:12:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// // function human(name){
+// //     function sayHi(){
+// //         console.log(`Hi I am ${name}`)
+// //     }
+// //     function sayHowYouFeel(){
+// //         console.log(`${name} is feeling good`)
+// //     }
+
+// //     return{
+// //           sayHi,
+// //           sayHowYouFeel
+// //     }
+
+// // }
+
+// // const sina = human('sina')
+// // const qoli = human('Qoli')
+
+// let x = 1;
+// let y = 2;
+// function function1(){
+//     // let x = 2;
+//     console.log(x )
+// }
+// function1()
+// function function2(){
+//     let y = 4
+//     console.log(y + y);
+// }
+// function2()
+
+const age = 29
+
+function printAge(){
+    console.log(age)
+}
+
+printAge()
